599b926ab9876a2ab7ca7a114720c47b3db6396e
AndriyMahera <andriymahera@gmail.com>
2017-05-14 17:36:06 +0300

    Fix RGR

--- Maherovsky/Maherovsky_RGR/TestingF1PSO.java
+++ Maherovsky/Maherovsky_RGR/TestingF1PSO.java
@@ -1,14 +1,14 @@
 import eva2.OptimizerFactory ;
-import eva2.server.go.problems.F1Problem ;
+import eva2.problems.F1Problem ;
 
 public class TestingF1PSO {
 	public static void main ( String [ ] args ) {
 		F1Problem f1 = new F1Problem ( ) ;
 		double [ ] sol ;
-		OptimizerFactory.setEvaluationTerminator ( 5 0 0 0 0 ) ;
+		OptimizerFactory.setEvaluationTerminator ( 50000 ) ;
 		sol = OptimizerFactory.optimizeToDouble ( OptimizerFactory .PSO, f1 , null ) ;
 		System.out.println( "Found solution : " ) ;
-		for ( int i =0; i<f 1 . getProblemDimension ( ) ; i++) System.out.print (sol[ i ] + " " ) ;
+		for ( int i =0; i<f1.getProblemDimension ( ) ; i++) System.out.print (sol[ i ] + " " ) ;
 		System.out.println ( ) ;
 	} ;
 }
--- Maherovsky/Maherovsky_RGR/TestingGAB1.java
+++ Maherovsky/Maherovsky_RGR/TestingGAB1.java
@@ -1,35 +1,40 @@
-import javautilBitSet;
-import eva2servergooperatorsselectionSelectXProbRouletteWheel;
-import eva2servergooperatorsterminatorsEvaluationTerminator;
-import eva2servergopopulationsPopulation;
-import eva2servergoproblemsB1Problem;
-import eva2servergostrategiesGeneticAlgorithm ;
-import eva2servermodulesGOParameters ;
+import java.util.BitSet;
+import eva2.problems.B1Problem;
+import eva2.OptimizerFactory;
+import eva2.optimization.OptimizationParameters;
+import eva2.optimization.operator.selection.InterfaceSelection;
+import eva2.optimization.operator.selection.SelectTournament;
+import eva2.optimization.operator.selection.SelectXProbRouletteWheel;
+import eva2.optimization.operator.terminators.*;
+import eva2.optimization.population.*;
+import eva2.optimization.strategies.GeneticAlgorithm;
+
 
 public class TestingGAB1 {
 	public static void main ( String [ ] args ) {
 		B1Problem b1 = new B1Problem ( ) ;
 		BitSet sol ;
 		// default go−parameter instance with a GA
-		GOParameters gaParams = OptimizerFactorystandardGA( b1 ) ;
+		OptimizationParameters gaParams = OptimizerFactory.standardGA(b1);
 		// add an evaluation terminator
-		gaParamssetTerminator (new EvaluationTerminator ( 1000 ) ) ;
+		gaParams.setTerminator (new EvaluationTerminator (1000) ) ;
 		// set a specific random seed
-		gaParamssetSeed ( 2342 ) ;
+		gaParams.setRandomSeed(2342);
+
 
 		// access the GA
-		GeneticAlgorithm ga = ( GeneticAlgorithm ) gaParamsgetOptimizer ( ) ;
+		GeneticAlgorithm ga = ( GeneticAlgorithm ) gaParams.getOptimizer();
 		// set no elitism
-		gasetElitism( false ) ;
+		ga.setElitism( false ) ;
 		// set roulette wheel selection
-		gasetParentSelection (new SelectXProbRouletteWheel ( ) ) ;
+		ga.setParentSelection(new SelectXProbRouletteWheel()) ;
 		// set population size 150
-		gasetPopulation(new Population ( 150 ) ) ;
+		ga.setPopulation(new Population ( 150 )) ;
 
 		// run optimization and print intermediate results to a file with given prefix
-		sol = OptimizerFactoryoptimizeToBinary( gaParams , "ga−opt−results " ) ;
-		Systemoutprintln( "Found solution : " ) ;
-		for ( int i =0; i<b1getProblemDimension ( ) ; i++) Systemoutpr int ( solget( i )+" " ) ;
-		Systemoutprintln( ) ;
+		sol = OptimizerFactory.optimizeToBinary( gaParams , "ga−opt−results " ) ;
+		System.out.println( "Found solution : " ) ;
+		for ( int i =0; i<b1.getProblemDimension ( ) ; i++) System.out.print(sol.get( i )+" " ) ;
+		System.out.println( ) ;
 	} ;
 }
