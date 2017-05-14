e30450504ca0a6b5a1ff3f13260ecb040e05077b
AndriyMahera <andriymahera@gmail.com>
2017-05-14 17:42:36 +0300

    Fix RGR

--- Maherovsky/Maherovsky_RGR/TestingGAB1.java
+++ Maherovsky/Maherovsky_RGR/TestingGAB1.java
@@ -1,40 +1,35 @@
-import java.util.BitSet;
-import eva2.problems.B1Problem;
-import eva2.OptimizerFactory;
-import eva2.optimization.OptimizationParameters;
-import eva2.optimization.operator.selection.InterfaceSelection;
-import eva2.optimization.operator.selection.SelectTournament;
-import eva2.optimization.operator.selection.SelectXProbRouletteWheel;
-import eva2.optimization.operator.terminators.*;
-import eva2.optimization.population.*;
-import eva2.optimization.strategies.GeneticAlgorithm;
-
+import javautilBitSet;
+import eva2servergooperatorsselectionSelectXProbRouletteWheel;
+import eva2servergooperatorsterminatorsEvaluationTerminator;
+import eva2servergopopulationsPopulation;
+import eva2servergoproblemsB1Problem;
+import eva2servergostrategiesGeneticAlgorithm ;
+import eva2servermodulesGOParameters ;
 
 public class TestingGAB1 {
 	public static void main ( String [ ] args ) {
 		B1Problem b1 = new B1Problem ( ) ;
 		BitSet sol ;
 		// default go−parameter instance with a GA
-		OptimizationParameters gaParams = OptimizerFactory.standardGA(b1);
+		GOParameters gaParams = OptimizerFactorystandardGA( b1 ) ;
 		// add an evaluation terminator
-		gaParams.setTerminator (new EvaluationTerminator (1000) ) ;
+		gaParamssetTerminator (new EvaluationTerminator ( 1000 ) ) ;
 		// set a specific random seed
-		gaParams.setRandomSeed(2342);
-
+		gaParamssetSeed ( 2342 ) ;
 
 		// access the GA
-		GeneticAlgorithm ga = ( GeneticAlgorithm ) gaParams.getOptimizer();
+		GeneticAlgorithm ga = ( GeneticAlgorithm ) gaParamsgetOptimizer ( ) ;
 		// set no elitism
-		ga.setElitism( false ) ;
+		gasetElitism( false ) ;
 		// set roulette wheel selection
-		ga.setParentSelection(new SelectXProbRouletteWheel()) ;
+		gasetParentSelection (new SelectXProbRouletteWheel ( ) ) ;
 		// set population size 150
-		ga.setPopulation(new Population ( 150 )) ;
+		gasetPopulation(new Population ( 150 ) ) ;
 
 		// run optimization and print intermediate results to a file with given prefix
-		sol = OptimizerFactory.optimizeToBinary( gaParams , "ga−opt−results " ) ;
-		System.out.println( "Found solution : " ) ;
-		for ( int i =0; i<b1.getProblemDimension ( ) ; i++) System.out.print(sol.get( i )+" " ) ;
-		System.out.println( ) ;
+		sol = OptimizerFactoryoptimizeToBinary( gaParams , "ga−opt−results " ) ;
+		Systemoutprintln( "Found solution : " ) ;
+		for ( int i =0; i<b1getProblemDimension ( ) ; i++) Systemoutpr int ( solget( i )+" " ) ;
+		Systemoutprintln( ) ;
 	} ;
 }
--- Maherovsky/Maherovsky_RGR/TestingPlusCMAES.java
+++ Maherovsky/Maherovsky_RGR/TestingPlusCMAES.java
@@ -1,12 +1,11 @@
-import eva2.OptimizerFactory;
-import eva2.optimization.OptimizationParameters;
-import eva2.optimization.individuals.AbstractEAIndividual;
-import eva2.optimization.operator.crossover.CrossoverESDefault;
-import eva2.optimization.operator.mutation.InterfaceMutation;
-import eva2.optimization.operator.mutation.MutateESCovarianceMatrixAdaption;
-import eva2.optimization.operator.terminators.EvaluationTerminator;
-import eva2.optimization.strategies.EvolutionStrategies;
-import eva2.problems.FM0Problem;
+import eva2.OptimizerFactory ;
+import eva2.server.go.individuals.AbstractEAIndividual ;
+import eva2.server.go.operators.crossover.CrossoverESDefault ;
+import eva2.server.go.operators.mutation.MutateESCovarianceMartixAdaption ;
+import eva2.server.go.operators.terminators.EvaluationTerminator ;
+import eva2.server.go.problems.FM0Problem;
+import eva2.server.go.strategies.EvolutionStrategies ;
+import eva2.server.modules.GOParameters ;
 
 public class TestingPlusCMAES {
 	public static void main ( String [ ] args ) {
@@ -14,27 +13,27 @@ public class TestingPlusCMAES {
 		FM0Problem fm0 = new FM0Problem ( ) ;
 		AbstractEAIndividual bestIndy ;
 		// create standard ES parameters
-		OptimizationParameters esParams = OptimizerFactory.standardES(fm0) ;
-		esParams.setTerminator(new EvaluationTerminator(2000)) ;
-		esParams.setRandomSeed(0);
+		GOParameters esParams = OptimizerFactory.standardES ( fm0 ) ;
+		esParams.setTerminator (new EvaluationTerminator ( 2 0 0 0 ) ) ;
+		// set a random seed based on system time
+		esParams.setSeed ( 0 ) ;
 		
-		// set evolutionary operators and probabilities
-		AbstractEAIndividual.setOperators(
-		fm0.getIndividualTemplate() ,
-		new MutateESCovarianceMatrixAdaption() , 0.9 ,
-		new CrossoverESDefault() , 0.1 ) ;
-	
+		// set e volutionary operators and probabilities
+		AbstractEAIndividual.setOperators (
+		fm0.getIndividualTemplat e ( ) ,
+		new MutateESCovarianceMartixAdaption ( ) , 0.9 ,
+		new CrossoverESDefault ( ) , 0.1 ) ;
+		
 		// access the ES
-		EvolutionStrategies es = (EvolutionStrategies)esParams.getOptimizer ( ) ;
+		EvolutionStrategieses = (EvolutionStrategies)esParams.getOptimizer ( ) ;
 		// set a (1+5) selection strategy
-		es.setMu(1);
-		es.setLambda(5);
-		es.setPlusStrategy(true);
+		es.setMu ( 1 ) ;
+		es.setLambda ( 5 ) ;
+		es.setPlusStrategy ( true ) ;
 		
 		// run optimization and retrieve winner individual
-		bestIndy = ( AbstractEAIndividual ) OptimizerFactory.optimizeToInd( esParams , null ) ;
+		bestIndy = ( AbstractEAIndividual ) OptimizerFactory.optimizeToInd ( esParams , null ) ;
 		System.out.println ( "Found solution : "
 		+ AbstractEAIndividual.getDefaultDataString ( bestIndy ) ) ;
 	} ;
 }
-
